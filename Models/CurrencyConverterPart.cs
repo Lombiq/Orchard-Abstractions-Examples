ab5286847cb329f5eaa7e9e14d11119478bcb42f
Piedone <piedone@pyrocenter.hu>
2013-11-05 00:14:46 +0100

    Currency converter is now a widget

--- Models/CurrencyConverterPart.cs
+++ Models/CurrencyConverterPart.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Lombiq.Abstractions.QuickParts;
+using Lombiq.Abstractions.QuickWidgets;
 using Orchard.Environment.Extensions;
 
 namespace Lombiq.Abstractions.Examples.Models
 {
     [OrchardFeature("Lombiq.Abstractions.Examples.QuickWidgets")]
-    public class CurrencyConverterPart : QuickPart
+    public class CurrencyConverterPart : QuickWidgetPart
     {
         public virtual string FromCurrencyCode { get; set; }
         public virtual string ToCurrencyCode { get; set; }
